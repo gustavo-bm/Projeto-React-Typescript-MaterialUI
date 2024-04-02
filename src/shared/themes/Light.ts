12c55c1764a8aa32e90b50a2f96a6122b8fca03e
gustavo-bm <g.bianchinimoraes@gmail.com>
2024-04-02 12:16:08 -0300

    manipulação de temas

---
+++ src/shared/themes/Light.ts
@@ -0,0 +1,24 @@
+import { createTheme } from '@mui/material'
+import { yellow, cyan } from '@mui/material/colors';
+// control + space abre as possibilidades
+// função que vem do material ui
+export const LightTheme = createTheme({
+    palette: {
+        primary: {
+            main: yellow[700],
+            dark: yellow[800],
+            light: yellow[500],
+            contrastText: '#ffffff'
+        },
+        secondary: {
+            main: cyan[700],
+            dark: cyan[800],
+            light: cyan[500],
+            contrastText: '#ffffff' /* branco */
+        },
+        background: {
+            default: '#f7f6f3',
+            paper: '#ffffff'
+        }
+    }
+});
