d579b334a039e106b7858b2f70b9a043094d2ea8
gustavo-bm <g.bianchinimoraes@gmail.com>
2024-04-08 19:00:41 -0300

    menu lateral feito

--- src/shared/contexts/ThemeContext.tsx
+++ src/shared/contexts/ThemeContext.tsx
@@ -1,48 +1,43 @@
-// somente através do contexto eu consigo compartilhar as informações do nosso tema
-import React, { createContext, useCallback, useContext, useMemo, useState } from 'react'
+import { createContext, useCallback, useContext, useMemo, useState } from 'react';
+import { ThemeProvider } from '@mui/material';
+import { Box } from '@mui/system';
 
-import { ThemeProvider } from '@emotion/react'
-import { Box } from '@mui/material'
-import { LightTheme } from '../themes/Light'
-import { DarkTheme } from '../themes/Dark'
+import { DarkTheme, LightTheme } from './../themes';
 
-interface ThemeContextData {
-    themeName: 'light' | 'dark'
-    toggleTheme: () => void
+interface IThemeContextData {
+    themeName: 'light' | 'dark';
+    toggleTheme: () => void;
 }
-// define quais propriedades são compartilhadas
 
-const ThemeContext = createContext({} as ThemeContextData)
+const ThemeContext = createContext({} as IThemeContextData);
 
 export const useAppThemeContext = () => {
-    return useContext(ThemeContext)
+    return useContext(ThemeContext);
 }
-
 interface IAppThemeProviderProps {
     children: React.ReactNode
 }
 export const AppThemeProvider: React.FC<IAppThemeProviderProps> = ({ children }) => {
-    const [themeName, setThemeName] = useState<'light' | 'dark'>('light')
+    const [themeName, setThemeName] = useState<'light' | 'dark'>('light');
 
     const toggleTheme = useCallback(() => {
-        setThemeName(oldThemeName => oldThemeName === 'light' ? 'dark' : 'light')
-    }, []) //array de dependencias
+        setThemeName(oldThemeName => oldThemeName === 'light' ? 'dark' : 'light');
+    }, []);
 
-    // alternar o tema
     const theme = useMemo(() => {
-        if (themeName === 'light') return LightTheme
+        if (themeName === 'light') return LightTheme;
 
-        return DarkTheme
-    }, [themeName])
+        return DarkTheme;
+    }, [themeName]);
+
 
     return (
-        <ThemeContext.Provider value={{ themeName, toggleTheme }} >
-            <ThemeProvider theme={DarkTheme}>
-                <Box width="100vw" height="100vh" bgcolor={theme.palette.background.default} >
+        <ThemeContext.Provider value={{ themeName, toggleTheme }}>
+            <ThemeProvider theme={theme}>
+                <Box width="100vw" height="100vh" bgcolor={theme.palette.background.default}>
                     {children}
                 </Box>
-
             </ThemeProvider>
         </ThemeContext.Provider>
-    )
+    );
 }
