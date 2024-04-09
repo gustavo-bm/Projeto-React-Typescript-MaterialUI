fe9cad48e65365a31a3ad0cdb4f06d7ec2ff3840
gustavo-bm <g.bianchinimoraes@gmail.com>
2024-04-09 15:46:28 -0300

    componente barra de ferramentas adicionado

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,10 +1,20 @@
+
+import { BarraDeFerramentas } from '../../shared/components/barra-de-ferramentas/BarraDeFerramentas';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 
 
 export const Dashboard = () => {
 
     return (
-        <LayoutBaseDePagina titulo='Página inicial' barraDeFerramentas={<>Barra de ferramentas</>}>
+        <LayoutBaseDePagina
+            titulo='Página inicial'
+            barraDeFerramentas={(
+                <BarraDeFerramentas
+                    mostrarInputBusca
+                    textoBotaoNovo='Nova'
+                />
+            )}
+        >
             Testando
         </LayoutBaseDePagina>
     );
