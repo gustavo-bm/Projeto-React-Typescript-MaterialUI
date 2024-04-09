95e3e4ba7fd5decc5d909ac62d277e12f424db79
gustavo-bm <g.bianchinimoraes@gmail.com>
2024-04-09 16:31:20 -0300

    responsividade nas ferramentas de detlahe

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,14 +1,18 @@
 import { FerramentasDeDetalhe } from '../../shared/components/ferramentas-de-detalhe/FerramentasDeDetalhe';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 
-
 export const Dashboard = () => {
 
     return (
         <LayoutBaseDePagina
             titulo='Página inicial'
             barraDeFerramentas={(
-                <FerramentasDeDetalhe mostrarBotaoSalvarEFechar mostrarBotaoSalvarEFecharCarregando />
+                <FerramentasDeDetalhe
+                    mostrarBotaoNovo
+                    mostrarBotaoSalvarEFechar
+                    mostrarBotaoSalvarEFecharCarregando
+                    mostrarBotaoVoltar={false}
+                />
             )}
         >
             Testando
