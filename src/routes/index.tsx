4895aa8009b8877aad0ae99c1c395c76eb03de3c
gustavo-bm <g.bianchinimoraes@gmail.com>
2024-04-09 11:19:36 -0300

    opções de menu no drawer configuradas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,32 @@
 import { Button } from '@mui/material';
-import { Routes, Route, Navigate } from "react-router-dom"
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import { useDrawerContext } from '../shared/contexts';
+import { useEffect } from 'react';
 
 export const AppRoutes = () => {
-    const { toggleDrawerOpen } = useDrawerContext();
+    const { toggleDrawerOpen, setDrawerOptions } = useDrawerContext();
+
+    useEffect(() => {
+        setDrawerOptions([
+            {
+                icon: 'home',
+                path: '/pagina-inicial',
+                label: 'Página inicial',
+            },
+            {
+                // opção teste
+                icon: 'star',
+                path: '/star',
+                label: 'Estrela',
+            }
+        ]);
+    }, []);
 
     return (
         <Routes>
             <Route path="/pagina-inicial" element={<Button variant='contained' color='primary' onClick={toggleDrawerOpen}>Toggle drawer</Button>} />
-
+            <Route path="/star" element={<Button variant='contained' color='primary' onClick={toggleDrawerOpen}>Toggle drawer</Button>} />
             <Route path="*" element={<Navigate to="/pagina-inicial" />} />
         </Routes>
     );
--- src/shared/contexts/DrawerContext.tsx
+++ src/shared/contexts/DrawerContext.tsx
@@ -1,9 +1,22 @@
 import { createContext, useCallback, useContext, useState } from 'react';
 
+interface IDrawerOption {
+    icon: string;
+    path: string;
+    label: string;
+}
+
 interface IDrawerContextData {
     isDrawerOpen: boolean;
     toggleDrawerOpen: () => void;
-};
+    drawerOptions: IDrawerOption[];
+    setDrawerOptions: (newDrawerOptions: IDrawerOption[]) => void;
+}
+
+interface IDrawerContextData {
+    isDrawerOpen: boolean;
+    toggleDrawerOpen: () => void;
+}
 
 const DrawerContext = createContext({} as IDrawerContextData);
 
@@ -13,17 +26,23 @@ export const useDrawerContext = () => {
 
 interface IDrawerProvider {
     children: React.ReactNode;
-};
+}
 
 export const DrawerProvider: React.FC<IDrawerProvider> = ({ children }) => {
+
+    const [drawerOptions, setDrawerOptions] = useState<IDrawerOption[]>([]);
     const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
     const toggleDrawerOpen = useCallback(() => {
         setIsDrawerOpen(oldDrawerOpen => !oldDrawerOpen);
     }, []);
 
+    const handleSetDrawerOptions = useCallback((newDrawerOptions: IDrawerOption[]) => {
+        setDrawerOptions(newDrawerOptions);
+    }, []);
+
     return (
-        <DrawerContext.Provider value={{ isDrawerOpen, toggleDrawerOpen }}>
+        <DrawerContext.Provider value={{ isDrawerOpen, drawerOptions, toggleDrawerOpen, setDrawerOptions: handleSetDrawerOptions }}>
             {children}
         </DrawerContext.Provider>
     );
