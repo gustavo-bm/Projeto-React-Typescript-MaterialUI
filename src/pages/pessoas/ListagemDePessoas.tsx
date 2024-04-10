4dcc1543a0032cb5ab872a9d43521eae04084f60
gustavo-bm <g.bianchinimoraes@gmail.com>
2024-04-09 23:20:35 -0300

    tabela de pessoas

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -1,35 +1,51 @@
-import { useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import { LinearProgress, Pagination, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow } from '@mui/material';
 import { useSearchParams } from 'react-router-dom';
 
-import { PessoasService } from '../../shared/services/api/pessoas/PessoasService';
+import { IListagemPessoa, PessoasService, } from '../../shared/services/api/pessoas/PessoasService';
 import { FerramentasDaListagem } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
-import { useDebounce } from '../../shared/hooks/UseDebounce';
-
+import { useDebounce } from '../../shared/hooks';
+import { Environment } from '../../shared/environment';
 
 
 export const ListagemDePessoas: React.FC = () => {
     const [searchParams, setSearchParams] = useSearchParams();
-    const { debounce } = useDebounce(3000, false);
+    const { debounce } = useDebounce();
+
+    const [rows, setRows] = useState<IListagemPessoa[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [totalCount, setTotalCount] = useState(0);
+
 
     const busca = useMemo(() => {
         return searchParams.get('busca') || '';
     }, [searchParams]);
 
+    const pagina = useMemo(() => {
+        return Number(searchParams.get('pagina') || '1');
+    }, [searchParams]);
+
 
     useEffect(() => {
+        setIsLoading(true);
 
         debounce(() => {
-            PessoasService.getAll(1, busca)
+            PessoasService.getAll(pagina, busca)
                 .then((result) => {
+                    setIsLoading(false);
+
                     if (result instanceof Error) {
                         alert(result.message);
                     } else {
                         console.log(result);
+
+                        setTotalCount(result.totalCount);
+                        setRows(result.data);
                     }
                 });
         });
-    }, [busca]);
+    }, [busca, pagina]);
 
 
     return (
@@ -40,11 +56,55 @@ export const ListagemDePessoas: React.FC = () => {
                     mostrarInputBusca
                     textoDaBusca={busca}
                     textoBotaoNovo='Nova'
-                    aoMudarTextoDeBusca={texto => setSearchParams({ busca: texto }, { replace: true })}
+                    aoMudarTextoDeBusca={texto => setSearchParams({ busca: texto, pagina: '1' }, { replace: true })}
                 />
             }
         >
+            <TableContainer component={Paper} variant="outlined" sx={{ m: 1, width: 'auto' }}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Ações</TableCell>
+                            <TableCell>Nome completo</TableCell>
+                            <TableCell>Email</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map(row => (
+                            <TableRow key={row.id}>
+                                <TableCell>Ações</TableCell>
+                                <TableCell>{row.nomeCompleto}</TableCell>
+                                <TableCell>{row.email}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
 
+                    {totalCount === 0 && !isLoading && (
+                        <caption>{Environment.LISTAGEM_VAZIA}</caption>
+                    )}
+
+                    <TableFooter>
+                        {isLoading && (
+                            <TableRow>
+                                <TableCell colSpan={3}>
+                                    <LinearProgress variant='indeterminate' />
+                                </TableCell>
+                            </TableRow>
+                        )}
+                        {(totalCount > 0 && totalCount > Environment.LIMITE_DE_LINHAS) && (
+                            <TableRow>
+                                <TableCell colSpan={3}>
+                                    <Pagination
+                                        page={pagina}
+                                        count={Math.ceil(totalCount / Environment.LIMITE_DE_LINHAS)}
+                                        onChange={(_, newPage) => setSearchParams({ busca, pagina: newPage.toString() }, { replace: true })}
+                                    />
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableFooter>
+                </Table>
+            </TableContainer>
         </LayoutBaseDePagina>
     );
 };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,9 +3,9 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 
 import { useDrawerContext } from '../shared/contexts';
 import {
-    Dashboard,
-    ListagemDeCidade,
+    Dashboard
 } from '../pages';
+import { ListagemDePessoas } from '../pages/pessoas/ListagemDePessoas';
 
 export const AppRoutes = () => {
     const { setDrawerOptions } = useDrawerContext();
@@ -18,9 +18,9 @@ export const AppRoutes = () => {
                 label: 'Página inicial',
             },
             {
-                icon: 'location_city',
-                path: '/cidades',
-                label: 'Cidades',
+                icon: 'people',
+                path: '/pessoas',
+                label: 'Pessoas',
             },
         ]);
     }, []);
@@ -29,8 +29,8 @@ export const AppRoutes = () => {
         <Routes>
             <Route path="/pagina-inicial" element={<Dashboard />} />
 
-            <Route path="/cidades" element={<ListagemDeCidade />} />
-            {/* <Route path="/cidades/detalhe/:id" element={<Dashboard />} /> */}
+            <Route path="/pessoas" element={<ListagemDePessoas />} />
+            {/* <Route path="/pessoas/detalhe/:id" element={<Dashboard />} /> */}
 
             <Route path="*" element={<Navigate to="/pagina-inicial" />} />
         </Routes>
